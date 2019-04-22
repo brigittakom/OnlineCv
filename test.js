e1b8e2fda621c80bf834f71ccbb4af37a868a95d
brigi <brigittacs3@gmail.com>
2019-04-22 18:56:31 +0200

    test Javascript

---
+++ test.js
@@ -0,0 +1,15 @@
+let age = 14;
+age = age + 4;
+
+if (age < 18) {
+    console.log('tu ne rentres pas')
+} else {
+    console.log('Bienvenue')
+}
+
+let bonbon = 100;
+
+while (bonbon > 0) {
+    bonbon = bonbon - 1;
+    console.log('nom nom, il en reste ' + bonbon)
+}
